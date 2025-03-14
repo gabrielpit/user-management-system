447056e3f235bd7de0bf47d3395a9395ae6d977d
gabrielpit <gabrielpit2k19@gmail.com>
2025-03-13 22:01:54 -0300

    feat: add UserController with CRUD operations

---
+++ src/main/java/com/gabriel/user_management/controller/UserController.java
@@ -0,0 +1,47 @@
+package com.gabriel.user_management.controller;
+
+import com.gabriel.user_management.model.User;
+import com.gabriel.user_management.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path ="/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<User> getAllUSers(){
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{email}")
+    public ResponseEntity<User> getUserByEmail(@PathVariable String email){
+        Optional<User> user = userService.getUserByEmail(email);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> saveUser(@RequestBody User user){
+        User savedUser = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable Long id){
+        userService.deleteUser(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+    
+}
--- src/main/java/com/gabriel/user_management/service/UserService.java
+++ src/main/java/com/gabriel/user_management/service/UserService.java
@@ -30,7 +30,7 @@ public class UserService {
         return userRepository.save(user);
     }
 
-    public void deleteUSer(Long id){
+    public void deleteUser(Long id){
         userRepository.deleteById(id);
     }
 }
