30657b48b5313f62c2a3acf405f08c1573beaf2e
gabrielpit <gabrielpit2k19@gmail.com>
2025-03-13 21:32:22 -0300

    feat: add UserService to handle business logic

---
+++ src/main/java/com/gabriel/user_management/service/UserService.java
@@ -0,0 +1,36 @@
+package com.gabriel.user_management.service;
+
+import com.gabriel.user_management.model.User;
+import com.gabriel.user_management.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserByEmail(String email){
+        return userRepository.findUserByEmail(email);
+    }
+
+    public User saveUser(User user){
+        return userRepository.save(user);
+    }
+
+    public void deleteUSer(Long id){
+        userRepository.deleteById(id);
+    }
+}
